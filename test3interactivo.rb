8615030ffdccb3bd3601cba782b2b875b288c435
profesor <profesor@ies.ies>
2013-09-24 10:34:41 +0100

    Second script, interactive

---
+++ test3interactivo.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby
+# encoding: UTF-8
+
+print "¿Qué acción desea realizar? ¿Crear o eliminar?"
+a=gets.chomp()
+
+if a=="crear" then
+	system("mkdir curso2")
+	system("mkdir curso2/add")
+	system("mkdir curso2/sgy")
+	system("mkdir curso2/srd")
+	system("chmod 700 curso2/*")
+	puts "¡Carpetas creadas!"
+
+elsif a=="eliminar" then
+	system("rm -r curso2")
+	puts "¡Carpetas eliminadas!"
+else
+	puts "No entiendo <"+a+">"
+end
